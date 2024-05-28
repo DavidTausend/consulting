fe92e15b4372ad25ce209514895b87e69572e791
DavidTausend <davidtausend-911@hotmail.com>
2024-05-28 15:47:08 +0000

    add description to the portfilio

--- consult/accounts/management/commands/create_portfolios.py
+++ consult/accounts/management/commands/create_portfolios.py
@@ -7,9 +7,46 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         portfolios = [
             {
-                "title": "aws-bootcamp-cruddur-2023",
-                "description": "Developed an twitter application with only cloud compones and using aws as main cloud provide.",
-                "image": "",
+                "title": "Cruddur",
+                "description": "Developed a Twitter application using AWS as the main cloud provider.",
+                "image": "static/portfolio/cruddur.webp",
+                "client_testimonial": ""
+            },
+             {
+                "title": "Terrahouses",
+                "description": "Developed an application where users can upload and share websites.",
+                "image": "static/portfolio/terrahouse.webp",
+                "client_testimonial": ""
+            },
+            {
+                "title": "Tausendlin",
+                "description": "An imaginative journey through Tausendlin, offering vibrant and diverse experiences.",
+                "image": "static/portfolio/tausendlin.webp",
+                "client_testimonial": ""
+            },
+            {
+                "title": "pokemon-memory-cards-game",
+                "description": "A fun and challenging Pokémon Memory Card Game for all ages with vibrant graphics.",
+                "image": "static/portfolio/pokemon.webp",
+                "client_testimonial": ""
+            },
+            {
+                "title": "Enigma",
+                "description": "A thrilling game where you decode messages and solve puzzles using the Enigma machine.",
+                "image": "static/portfolio/enigma_machine.webp",
+                "client_testimonial": ""
+            },
+            {
+                "title": "Consulting",
+                "description": "A comprehensive consulting service tailored to meet client needs.",
+                "image": "static/portfolio/enigma_machine.webp",
+                "client_testimonial": ""
+            },
+            {
+                "title": "Predictive Analytics",
+                "description": "Coming soon.",
+                "image": "static/portfolio/predictive_analytics.webp",
+                "client_testimonial": ""
             }
         ]
 
